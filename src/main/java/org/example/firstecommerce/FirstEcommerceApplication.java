0391174b77528ddaa34732db3507f56bc6cd38e7
Lesio <e.leszek.one@gmail.com>
2025-03-11 22:12:00 +0100

    Initial commit

---
+++ src/main/java/org/example/firstecommerce/FirstEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.example.firstecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/firstecommerce/FirstEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
