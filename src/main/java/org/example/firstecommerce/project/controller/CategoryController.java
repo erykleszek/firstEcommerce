cc7ebf1680e338089c988925e0e7f255f07fbf29
Lesio <e.leszek.one@gmail.com>
2025-03-12 20:21:28 +0100

    adding new categories

--- src/main/java/org/example/firstecommerce/project/controller/CategoryController.java
+++ src/main/java/org/example/firstecommerce/project/controller/CategoryController.java
@@ -1,22 +1,32 @@
 package org.example.firstecommerce.project.controller;
 
+
+
 import org.example.firstecommerce.project.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/public/categories") // Podstawowa ścieżka
+@CrossOrigin(origins = "*")
+
 public class CategoryController {
 
     private List<Category> categories = new ArrayList<>();
 
-    @GetMapping("/api/public/categories")
-    public List<Category> getCategories() {
+    // Obsłuży HTTP GET na /api/public/categories
+    @GetMapping
+    public List<Category> getAllCategories(){
         return categories;
     }
 
-
+    // Obsłuży HTTP POST na /api/public/categories
+    @PostMapping
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully: " + category.getCategoryName();
+    }
 
 }
--- src/main/java/org/example/firstecommerce/project/model/Category.java
+++ src/main/java/org/example/firstecommerce/project/model/Category.java
@@ -4,8 +4,20 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
+
+    public Category() {
+    }
+
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
 
@@ -15,13 +27,5 @@ public class Category {
 
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
     }
 }
