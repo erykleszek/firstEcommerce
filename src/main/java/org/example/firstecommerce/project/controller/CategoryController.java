dab3458c478190c5a4dc7db5b327231a92c71a73
Lesio <e.leszek.one@gmail.com>
2025-06-13 00:07:45 +0200

    add status information's handler

--- src/main/java/org/example/firstecommerce/project/controller/CategoryController.java
+++ src/main/java/org/example/firstecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package org.example.firstecommerce.project.controller;
 
 
 
+import jakarta.validation.Valid;
 import org.example.firstecommerce.project.model.Category;
 import org.example.firstecommerce.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class CategoryController {
     }
 
     @PostMapping("/api/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
