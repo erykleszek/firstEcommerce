8cecdd7bfb9cf186261f84c21d2e131ca33a3a16
Eryk Leszek <e.leszek.one@gmail.com>
2026-01-13 00:42:03 +0100

    Saving Products to a Category

--- src/main/java/org/example/firstecommerce/project/service/ProductService.java
+++ src/main/java/org/example/firstecommerce/project/service/ProductService.java
@@ -1,4 +1,8 @@
 package org.example.firstecommerce.project.service;
 
+import org.example.firstecommerce.project.model.Product;
+import org.example.firstecommerce.project.payload.ProductDTO;
+
 public interface ProductService {
+    ProductDTO addProduct(Long categoryId, Product product);
 }
--- src/main/java/org/example/firstecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/org/example/firstecommerce/project/service/ProductServiceImpl.java
@@ -1,7 +1,36 @@
 package org.example.firstecommerce.project.service;
 
+import org.example.firstecommerce.project.Exceptions.ResourceNotFoundException;
+import org.example.firstecommerce.project.model.Category;
+import org.example.firstecommerce.project.model.Product;
+import org.example.firstecommerce.project.payload.ProductDTO;
+import org.example.firstecommerce.project.repositories.CategoryRepository;
+import org.example.firstecommerce.project.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+        product.setImage("default.png");
+        product.setCategory(category);
+        double specialPrice = product.getPrice() - ((product.getDiscount() * 0.01) * product.getPrice());
+        product.setPrice(specialPrice);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
